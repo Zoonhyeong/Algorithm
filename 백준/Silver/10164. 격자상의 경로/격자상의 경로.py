0429ebafe13c7edffa0009de1949f1bf695a6c74
Zoonhyeong <113488930+Zoonhyeong@users.noreply.github.com>
2025-02-17 10:40:11 +0900

    [Silver II] Title: 격자상의 경로, Time: 36 ms, Memory: 34668 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
@@ -1,34 +1,20 @@
-# 10164 격자상의 경로
-from itertools import combinations
-
-def combination(a, b, c):
-    min_num = 0
-    result1 = 1
-    result2 = 1
-
-    if b > c :
-        min_num = c
-    else :
-        min_num = b
-    
-    for i in range(min_num):
-        result1 *= (a - i)
-    for i in range(min_num):
-        result2 *= (min_num - i)
-
-    return result1 // result2
+from math import comb
 
 N, M, K = map(int, input().split())
 
 if K == 0:
-    print(combination(N+M-2, N-1, M-1))
+    print(comb(N + M - 2, N - 1))
 else:
+    # K를 0-based index로 변경
+    K -= 1
+    
     row1 = K // M
-    column1 = K % M - 1
-    rst1 = combination(row1+column1, row1, column1)
+    column1 = K % M
+
+    rst1 = comb(row1 + column1, row1)
 
     row2 = N - 1 - row1
     column2 = M - 1 - column1
-    rst2 = combination(row2+column2, row2, column2)
+    rst2 = comb(row2 + column2, row2)
 
     print(rst1 * rst2)
