f6bf11cf60fc2613a3324a6b87b33353eaa503ff
Zoonhyeong <113488930+Zoonhyeong@users.noreply.github.com>
2025-04-30 17:52:10 +0900

    [Silver I] Title: 트리 순회, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,33 @@
+# 1991 트리 순회
+
+tree = {}
+
+N = int(input())
+
+for _ in range(N):
+    root, left, right = map(str, input().split())
+    tree[root] = [left, right]
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')  # root
+        preorder(tree[root][0])  # left
+        preorder(tree[root][1])  # right
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])  # left
+        print(root, end='')  # root
+        inorder(tree[root][1])  # right
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])  # left
+        postorder(tree[root][1])  # right
+        print(root, end='')  # root
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
