c18d0ae212193847b2d54dd11e86b0faa862381f
Zoonhyeong <113488930+Zoonhyeong@users.noreply.github.com>
2025-07-27 22:28:25 +0900

    [Gold V] Title: 풍선 맞추기, Time: 452 ms, Memory: 119744 KB -BaekjoonHub

---
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+ballons = list(map(int, input().split()))
+flag = [0] * 1000001
+ans = 0
+for i in range(N):
+    if flag[ballons[i]] == 0:
+        ans += 1
+        flag[ballons[i] - 1] += 1
+    else:
+        flag[ballons[i]] -= 1
+        flag[ballons[i] - 1] += 1
+
+print(ans)
