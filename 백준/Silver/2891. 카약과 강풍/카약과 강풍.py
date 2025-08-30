eb13f1f2e70fb96dbe2db99e3575d624f923193d
Zoonhyeong <113488930+Zoonhyeong@users.noreply.github.com>
2025-08-30 19:08:29 +0900

    [Silver IV] Title: 카약과 강풍, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2891. 카약과 강풍/카약과 강풍.py
+++ 백준/Silver/2891. 카약과 강풍/카약과 강풍.py
@@ -20,34 +20,11 @@ R_num = set(R_num) - T_num
 
 result = 0
 for i in S_num:
-    if i - 1 in R_num:
-        R_num.remove(i - 1)
-    elif i + 1 in R_num:
-        R_num.remove(i + 1)
+    if i-1 in R_num:
+        R_num.remove(i-1)
+    elif i+1 in R_num:
+        R_num.remove(i+1)
     else:
         result += 1
-
-    """
-    if i == 0:
-        if i+1 in R_num:
-            R_num.remove(i+1)
-            continue
-        else:
-            result += 1
-    elif i == N-1:
-        if i-1 in R_num:
-            R_num.remove(i-1)
-            continue
-        else:
-            result += 1
-    else:
-        if i+1 in R_num:
-            R_num.remove(i+1)
-            continue
-        elif i-1 in R_num:
-            R_num.remove(i-1)
-            continue
-        else:
-            result += 1"""
 
 print(result)
