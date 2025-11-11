a0b99fc0e073a0e34ec79b271dbafbfd9a1b921a
Zoonhyeong <113488930+Zoonhyeong@users.noreply.github.com>
2025-11-11 23:26:07 +0900

    [Silver IV] Title: 로프, Time: 2476 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,19 @@
+# 2217 로프
+# k개 로프, 중량 w -> 각각엔 w/k
+
+N = int(input())
+
+rope = []
+for _ in range(N):
+    rope.append(int(input()))
+
+rope.sort()
+
+max_weight = 0
+
+for i in range(N):
+    weight = rope[i] * (N-i)
+    if weight > max_weight:
+        max_weight = weight
+
+print(max_weight)
