df8d53c28cd97c7c8cec9646289730e24e41155b
Zoonhyeong <113488930+Zoonhyeong@users.noreply.github.com>
2026-03-01 17:39:48 +0900

    [Gold II] Title: 보석 도둑, Time: 1612 ms, Memory: 92968 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -26,6 +26,7 @@ for bag in bags:
     while gems and gems[0][0] <= bag: # 가방보다 작으면 보석을 임시 힙에다가 저장
         heapq.heappush(temp_heapq, -gems[0][1]) # 최대 힙을 구현해야하니까 음수를 붙임
         heapq.heappop(gems) # 보석 이미 임시힙에 박아놨으니까 원래 리스트에서 제거
+        # gems.pop(0) -> 시간초과 뜸
     if temp_heapq:
         max_price += -heapq.heappop(temp_heapq)
 
